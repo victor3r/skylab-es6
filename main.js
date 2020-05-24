fe50a79c0cc439763b27a27ef378cf22302dcbe6
victor <victorabelo1697@gmail.com>
2020-05-23 22:34:33 -0300

    Const & Let

--- main.js
+++ main.js
@@ -1,38 +1,58 @@
-class List {
-  constructor() {
-    this.data = [];
-  }
+// class List {
+//   constructor() {
+//     this.data = [];
+//   }
 
-  add(data) {
-    this.data.push(data);
-    console.log(this.data);
+//   add(data) {
+//     this.data.push(data);
+//     console.log(this.data);
+//   }
+// }
+
+// class TodoList extends List {
+//   constructor() {
+//     super();
+
+//     this.usuario = 'Diego';
+//   }
+
+//   mostraUsuario() {
+//     console.log(this.usuario);
+//   }
+// }
+
+// const MinhaLista = new TodoList();
+
+// document.getElementById('novotodo').onclick = () => {
+//   MinhaLista.add('Novo todo');
+// }
+
+// MinhaLista.mostraUsuario();
+
+// class Matematica {
+//   static soma(a, b) {
+//     return a + b;
+//   }
+// }
+
+// console.log(Matematica.soma(1, 2));
+
+const usuario = { nome: 'Diego' };
+
+usuario.nome = 'Victor';
+
+console.log(usuario);
+
+function teste(x) {
+  let y = 2;
+
+  if (x > 5) {
+    let y = 4;
+
+    console.log(x, y);
   }
 }
 
-class TodoList extends List {
-  constructor() {
-    super();
+console.log(y);
 
-    this.usuario = 'Diego';
-  }
-
-  mostraUsuario() {
-    console.log(this.usuario);
-  }
-}
-
-const MinhaLista = new TodoList();
-
-document.getElementById('novotodo').onclick = () => {
-  MinhaLista.add('Novo todo');
-}
-
-MinhaLista.mostraUsuario();
-
-class Matematica {
-  static soma(a, b) {
-    return a + b;
-  }
-}
-
-console.log(Matematica.soma(1, 2));
+teste(10);
