17210df8fbabdb7a525d900f447c723b7f280ab4
victor <victorabelo1697@gmail.com>
2020-05-23 22:18:33 -0300

    Classes

--- main.js
+++ main.js
@@ -1,11 +1,38 @@
-alert('Teste');
-
-class Teste {
-  metodo() {
-
+class List {
+  constructor() {
+    this.data = [];
   }
 
-  outro() {
-
+  add(data) {
+    this.data.push(data);
+    console.log(this.data);
   }
 }
+
+class TodoList extends List {
+  constructor() {
+    super();
+
+    this.usuario = 'Diego';
+  }
+
+  mostraUsuario() {
+    console.log(this.usuario);
+  }
+}
+
+const MinhaLista = new TodoList();
+
+document.getElementById('novotodo').onclick = () => {
+  MinhaLista.add('Novo todo');
+}
+
+MinhaLista.mostraUsuario();
+
+class Matematica {
+  static soma(a, b) {
+    return a + b;
+  }
+}
+
+console.log(Matematica.soma(1, 2));
