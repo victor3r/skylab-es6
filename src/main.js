6778907c870056dd4b5b79604da0e74691dd8c85
victor <victorabelo1697@gmail.com>
2020-05-27 15:29:44 -0300

    Render em tela

--- src/main.js
+++ src/main.js
@@ -180,6 +180,7 @@ class App {
     this.repositories = [];
 
     this.formEl = document.getElementById('repo-form');
+    this.listEl = document.getElementById('repo-list');
 
     this.registerHandlers();
   }
@@ -197,7 +198,33 @@ class App {
       html_url: 'http://github.com/rocketseat/rocketseat.com.br'
     });
 
-    console.log(this.repositories);
+    this.render();
+  }
+
+  render() {
+    this.listEl.innerHTML = '';
+    this.repositories.forEach(repo => {
+      let imgEl = document.createElement('img');
+      imgEl.setAttribute('src', repo.avatar_url);
+
+      let titleEl = document.createElement('strong');
+      titleEl.appendChild(document.createTextNode(repo.name));
+
+      let descriptionEl = document.createElement('p');
+      descriptionEl.appendChild(document.createTextNode(repo.description));
+
+      let linkEl = document.createElement('a');
+      linkEl.setAttribute('target', '_blank');
+      linkEl.appendChild(document.createTextNode('Acessar'));
+
+      let listItemEl = document.createElement('li');
+      listItemEl.appendChild(imgEl);
+      listItemEl.appendChild(titleEl);
+      listItemEl.appendChild(descriptionEl);
+      listItemEl.appendChild(linkEl);
+
+      this.listEl.appendChild(listItemEl);
+    });
   }
 }
 
