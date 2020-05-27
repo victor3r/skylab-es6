994e635da7cc55f132903ea560eeef29dd3b84f8
victor <victorabelo1697@gmail.com>
2020-05-27 15:09:27 -0300

    Adicionar repositórios

--- src/main.js
+++ src/main.js
@@ -160,17 +160,45 @@
 
 // executaPromise();
 
-import axios from 'axios';
+// import axios from 'axios';
 
-class Api {
-  static async getUserInfo(username) {
-    try {
-      const response = await axios.get(`https://api.github.com/users/${username}`);
-      console.log(response);
-    } catch (err) {
-      console.warn('Erro na API');
-    }
+// class Api {
+//   static async getUserInfo(username) {
+//     try {
+//       const response = await axios.get(`https://api.github.com/users/${username}`);
+//       console.log(response);
+//     } catch (err) {
+//       console.warn('Erro na API');
+//     }
+//   }
+// }
+
+// Api.getUserInfo('victor3r');
+
+class App {
+  constructor() {
+    this.repositories = [];
+
+    this.formEl = document.getElementById('repo-form');
+
+    this.registerHandlers();
+  }
+
+  registerHandlers() {
+    this.formEl.onsubmit = event => this.addRepository(event);
+  }
+  addRepository(event) {
+    event.preventDefault();
+
+    this.repositories.push({
+      name: 'rocketseat.com.br',
+      description: 'Tire a sua ideia do papel e dê vida à sua startup',
+      avatar_url: 'https://avatars0.githubusercontent.com/u/28929274?v=4',
+      html_url: 'http://github.com/rocketseat/rocketseat.com.br'
+    });
+
+    console.log(this.repositories);
   }
 }
 
-Api.getUserInfo('victor3r');
+new App();
